ffc3e62dbde1d3c8141249e70872cb3b9db4b6a1
SarahGartner <sarah.gartner97@gmail.com>
2021-01-04 21:32:46 +0100

    abfrage ob teile von kuehlgerät undefined sind

--- webapp/src/app/pages/fridges/fridges.component.ts
+++ webapp/src/app/pages/fridges/fridges.component.ts
@@ -15,18 +15,22 @@ export class FridgesComponent implements OnInit {
     this.http.post(this.url + 'fridges/ByUser', this.userId).toPromise().then(data => {
       for (let key in data)
         if (data.hasOwnProperty(key)) {
-          const fridge = {
-            name: data[key]['name'],
-            configured: data[key]['name'] != null ? true : false,
-            open: false,
-            mac: data[key]['_id'],
-            fridgeId: data[key]['fridgeId'],
-            minTemp: data[key]['minTemperature']['$numberDecimal'],
-            maxTemp: data[key]['maxTemperature']['$numberDecimal'],
-            minHum: data[key]['minHumidity']['$numberDecimal'],
-            maxHum: data[key]['maxHumidity']['$numberDecimal']
+          try {
+            const fridge = {
+              name: data[key] === null || typeof data[key]['name'] === 'undefined' ? "" : data[key]['name'],
+              configured: data[key]['name'] == "" || data[key]['name'] == null || typeof data[key]['name'] === 'undefined' ? false : true,
+              open: false,
+              mac: data[key]['_id'],
+              fridgeId: data[key] != null && typeof data[key]['fridegeId'] !== 'undefined' ? data[key]['fridgeId'] : "",
+              minTemp: data[key] != null && typeof data[key]['minTemperature'] !== 'undefined' ? data[key]['minTemperature']['$numberDecimal'] : "",
+              maxTemp: data[key] != null && typeof data[key]['maxTemperature'] !== 'undefined' ? data[key]['maxTemperature']['$numberDecimal'] : "",
+              minHum: data[key] != null && typeof data[key]['minHumidity'] !== 'undefined' ? data[key]['minHumidity']['$numberDecimal'] : null,
+              maxHum: data[key] != null && typeof data[key]['maxHumidity'] !== 'undefined' ?['maxHumidity']['$numberDecimal'] : null
+            }
+            this.fridgeNames.push(fridge);
+          } catch (err) {
+            console.log(err);
           }
-          this.fridgeNames.push(fridge);
         }
     })
     console.log(this.fridgeNames);
