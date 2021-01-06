8c324b72506e131aa678e937e39514ef3d4c21ea
SarahGartner <sarah.gartner97@gmail.com>
2021-01-06 17:35:32 +0100

    update fridge geht

--- webapp/src/app/pages/fridges/fridges.component.ts
+++ webapp/src/app/pages/fridges/fridges.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core'
 import { HttpClient } from '@angular/common/http';
-// import { AppComponent } from '../../app.component';
 
 @Component({
   selector: 'app-fridges',
   templateUrl: './fridges.component.html',
   styleUrls: ['./fridges.component.scss']
 })
+
 export class FridgesComponent implements OnInit {
   url = `https://kuehlschrankmonitoringapi.azurewebsites.net/`;
   fridgeNames = [];
-  userId = { userId: 3 };
+  userId = { userId: 201508 };
+
   constructor(private http: HttpClient) {
     this.http.post(this.url + 'fridges/ByUser', this.userId).toPromise().then(data => {
       for (let key in data)
@@ -28,15 +29,46 @@ export class FridgesComponent implements OnInit {
               maxHum: data[key] != null && typeof data[key]['maxHumidity'] !== 'undefined' ? data[key]['maxHumidity']['$numberDecimal'] : ""
             }
             this.fridgeNames.push(fridge);
+            console.log("original befüllt");
           } catch (err) {
             console.log(err);
           }
         }
     })
-    console.log(this.fridgeNames);
   }
 
   ngOnInit() {
   }
 
+  fridgeUpdate(item) {
+    console.log("update");
+    const name = (<HTMLInputElement>document.getElementById(item + '.name')).value;
+    const fridgeId = (<HTMLInputElement>document.getElementById(item + '.fridgeId')).value;
+    const minTemp = (<HTMLInputElement>document.getElementById(item + '.minTemp')).value;
+    const maxTemp = (<HTMLInputElement>document.getElementById(item + '.maxTemp')).value;
+    const minHum = (<HTMLInputElement>document.getElementById(item + '.minHum')).value;
+    const maxHum = (<HTMLInputElement>document.getElementById(item + '.maxHum')).value;
+    name != "" && (this.fridgeNames[item].name = name);
+    fridgeId != "" && (this.fridgeNames[item].fridgeId = fridgeId);
+    minTemp != "" && (this.fridgeNames[item].minTemp = minTemp);
+    maxTemp != "" && (this.fridgeNames[item].maxTemp = maxTemp);
+    minHum != "" && (this.fridgeNames[item].minHum = minHum);
+    maxHum != "" && (this.fridgeNames[item].maxHum = maxHum);
+    const kuehlschrank = {
+      _id: this.fridgeNames[item]['mac'],
+      name: name,
+      fridgeId: fridgeId,
+      minTemperature: minTemp,
+      maxTemperature: maxTemp,
+      minHumidity: minHum,
+      maxHumidity: maxHum,
+    }
+    try{
+    this.http.post(this.url + 'fridges/Update', kuehlschrank).toPromise().then(data => {
+      console.log(data);
+    });
+  } catch (err){
+    console.log(err);
+  }
+  }
 }
